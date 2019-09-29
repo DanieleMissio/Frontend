44d6d5cea3436b86e95ff75dc57e0c192ca2ab6b
daniele.missio <danielemissio0907@gmail.com>
2019-09-29 17:07:51 -0300

    Arquivo atualizado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using frontend.Models;
+using telas.Models;
 
-namespace frontend.Controllers
+namespace telas.Controllers
 {
     public class HomeController : Controller
     {
