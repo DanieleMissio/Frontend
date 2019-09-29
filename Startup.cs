0db4b960e3fe387f2f7676bcf21ef365be0ca52b
daniele.missio <danielemissio0907@gmail.com>
2019-09-29 18:20:59 -0300

    Atualizado

--- Startup.cs
+++ Startup.cs
@@ -9,10 +9,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using frontend.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using UseMySql.Data.EntityFrameworkCore.Extensions;
 
 namespace frontend
 {
