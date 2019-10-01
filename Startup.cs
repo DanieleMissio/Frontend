ca3ba3c25055d589bc15a37b5b18318e5114c127
daniele.missio <danielemissio0907@gmail.com>
2019-10-01 20:48:37 -0300

    Projeto atualizado e versão correta

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace frontend
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseMySql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
